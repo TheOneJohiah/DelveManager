8e1a4643f3d5e24c9354bc9089566923fe31258a
Shallow Assumptions <mudawiabubaker@gmail.com>
2023-09-02 04:40:41 +0100

    renamed populate

--- populate.py
+++
@@ -1,9 +0,0 @@
-# Witness my jank!
-import awakened as aw;
-
-# Example usage
-ac = aw.Awakened(name='Teston Lautts',attributes = [10, 10, 10, 10, 10, 60], level = 20, character_class = aw.worker)
-ac.add_skill(aw.intrinsic_clarity, starting_level=10)
-ac.add_skill(aw.intrinsic_focus, starting_level=10)
-ac.add_experience(2000)  # Set the character's current experience
-ac.printCharSheet()
